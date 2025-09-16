4c7cc523ab0080c6db7efb238cc9f81b4f7d513c
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2025-09-15 19:53:50 -0500

    Se agrega clase Ciclista, cotrarrelojosta y Escalador

---
+++ src/Contrarrelojista.java
@@ -0,0 +1,33 @@
+import Ciclista.Ciclista;
+
+public class Contrarrelojista extends Ciclista{
+
+    //Variables
+    private double velocidadMaxima;
+
+    //Constructor
+    public Contrarrelojista (int id, String nombre, double velocidadMaxima){
+        super(id, nombre);
+        this.velocidadMaxima = velocidadMaxima;       
+    }
+
+    public double getVelocidadMaxima (){
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima (double velocidadMaxima){
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    @Override
+    public String imprimirTipo(){
+        return "El tipo es Contrarrelojista";
+    }
+
+    public void imprimirDatos (){
+        super.imprimirDatos();
+        System.out.println("Velocidad Maxima : " + this.velocidadMaxima );
+    }
+    
+}
+
